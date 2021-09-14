558702e807d11ef69b211a1e698e41590dc4ad89
MargaritaCa <busy0001@algonquinlive.com>
2021-09-14 16:30:54 -0400

    finished version

--- 01-meeting-reminder/setup/src/data.js
+++ 01-meeting-reminder/setup/src/data.js
@@ -2,35 +2,40 @@ export default [
   {
     id: 1,
     name: 'Bertie Yates',
-    age: 29,
+    dayOfWeek: 'Monday',
+    time:'10:00am UTC',
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
   },
   {
     id: 2,
     name: 'Hester Hogan',
-    age: 32,
+    dayOfWeek: 'Monday',
+    time:'1:30pm UTC',
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
   },
   {
     id: 3,
     name: 'Larry Little',
-    age: 36,
+    dayOfWeek: 'Wednesday',
+    time:'4:00pm UTC',
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
   },
   {
     id: 4,
     name: 'Sean Walsh',
-    age: 34,
+    dayOfWeek: 'Thursday',
+    time:'9:30am UTC',
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
   },
   {
     id: 5,
     name: 'Lola Gardner',
-    age: 29,
+    dayOfWeek: 'Friday',
+    time:'11:30am UTC',
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
   },
